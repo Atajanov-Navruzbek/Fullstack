a95bc74ba08a5c2e1e3c0956354034a021f8ef52
Atajanov-Navruzbek <atazanovazarina07@gmail.com>
2026-06-25 12:33:49 +0500

    Add files via upload

--- fullstack/tasksdars18/task18.js
+++ fullstack/tasksdars18/task18.js
@@ -2,10 +2,20 @@
 // Massivdan yig'indisi berilgan K ga teng bo'lgan barcha juftliklarni toping. Misol: ([1,2,3,4],
 // 5) -> [[1,4],[2,3]]
 
-function sumTwoson(arr){
-    let sum = 0;
-    for (let i of arr){
-        console.log(i,[i]+1)
+function sumTwoson(arr,k){
+    let sum = [];
+    for (let i =0;i<arr.length;i++){
+        for(let j=1;j<arr.length;j++){
+            if(arr[i]+arr[j]=== k){
+                sum.push([arr[i],arr[j]])
+                if (sum.includes(arr[j],arr[i])){
+                    sum.pop(arr[j],arr[i])
+                }
+                
+            }
+        }
     }
+    
+    return sum
 };
 console.log(sumTwoson([1,2,3,4],5))
